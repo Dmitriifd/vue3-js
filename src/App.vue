e68b27b518316a6e13a19dc6b7bf9c999b9b5544
Dmitriifd <idimka9@gmail.com>
2023-04-14 15:19:18 +0500

    отрисовка поста в цикле

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,40 @@ export default {
   // Описание моделей
   data() {
     return {
-      likes: 0,
-      dislikes: 0
+      posts: [
+        {id: 1, title: 'JavaScript 1', body: 'Описание поста 1'},
+        {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+      ]
     }
   },
   // Функции объявляются в поле methods
   methods: {
-    addLike() {
-      this.likes += 1;
-    },
-    addDislike() {
-      this.dislikes += 1;
-    },
+
   }
 };
 </script>
 
 <template>
   <div>
-    <!-- Слушатели событий v-on -->
-    <button v-on:click="addLike">Like</button>
-    <!-- Короткая запись -->
-    <button @click="addDislike">Dislike</button>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
+    </div>
   </div>
-  <div>Кол-во лайков {{ likes }}</div>
-  <div>Кол-во дизлайков {{ dislikes }}</div>
 </template>
 
+<!-- Флаг scoped - говорит о том что стили будут применены только к этому компоненту -->
 <style>
+  * {
+    box-sizing: border-box;
+  }
+
+  .post {
+    padding: 15px;
+    border: 1px solid blueviolet;
+  }
+  .post:not(:last-child) {
+    margin-bottom: 15px;
+  }
 </style>
