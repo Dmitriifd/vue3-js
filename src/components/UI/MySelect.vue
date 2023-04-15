f51164180677a49918ac35339632458b7f533516
Dmitriifd <idimka9@gmail.com>
2023-04-15 20:53:01 +0500

    сортировка постов watch

--- src/components/UI/MySelect.vue
+++ src/components/UI/MySelect.vue
@@ -1,14 +1,12 @@
 <template>
-  <select :model-value="modelValue" @change="changeOption">
-    <option disabled value="">Выберите из списка</option>
+  <select :model-value="modelValue" @change="changeOption" class="select">
+    <option selected disabled value="">Выберите из списка</option>
     <option v-for="option in options" :key="option.value" :value="option.value">
       {{ option.name }}
     </option>
   </select>
 </template>
 <script>
-
-
 export default {
   name: 'my-select',
   props: {
@@ -22,10 +20,19 @@ export default {
   },
   methods: {
     changeOption(event) {
-      this.$emit('update:modelValue', event.target.value)
+      this.$emit('update:modelValue', event.target.value);
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.select {
+  border-radius: 8px;
+  border: 1px solid #f9f9f9;
+  padding: 0.6em 1.2em;
+  font-size: 1rem;
+  font-family: inherit;
+  cursor: pointer;
+}
+</style>
