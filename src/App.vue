3f51422541f4515a9152f569cdad05942767f639
Dmitriifd <idimka9@gmail.com>
2023-04-15 15:49:04 +0500

    работа с сервером, получение постов

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <!-- Модификаторы v-model (trim - удаляет пробелы), number, lazy -->
-    <input type="text" v-model.trim="modificatorValue">
+    <my-button @click="fetchPosts">Получить посты</my-button>
     <my-button @click="showDialog">Создать пост</my-button>
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
@@ -17,6 +16,7 @@
 <script>
 import PostForm from './components/PostForm.vue';
 import PostList from './components/PostList.vue';
+import axios from 'axios';
 
 export default {
   // Регистрация компонентов
@@ -27,11 +27,7 @@ export default {
 
   data() {
     return {
-      posts: [
-        { id: 1, title: 'JavaScript 1', body: 'Описание поста 1' },
-        { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
-        { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' }
-      ],
+      posts: [],
       dialogVisible: false,
       modificatorValue: ''
     };
@@ -46,6 +42,14 @@ export default {
     },
     showDialog() {
       this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        this.posts = response.data
+      } catch (e) {
+        alert('Error')
+      }
     }
   }
 };
