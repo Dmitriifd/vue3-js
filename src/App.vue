2008794de4c68dd5b95e645003a19aefc35c95e7
Dmitriifd <idimka9@gmail.com>
2023-04-15 22:15:20 +0500

    поиск постов

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
+    <my-input v-model="searchQuery" placeholder="Поиск..."/>
     <div class="app__btns">
       <my-button @click="showDialog">Создать пост</my-button>
       <my-select v-model="selectedSort" :options="sortOptions" />
@@ -11,7 +12,7 @@
     <!-- Передача пропсов -->
     <!-- <post-list v-bind:posts="posts"/> -->
     <!-- короткая запись -->
-    <post-list :posts="sortedPosts" @remove="removePost" v-if="!isPostLoading" />
+    <post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostLoading" />
     <!-- <div v-else>Loading...</div> -->
      <div class="load-wrapp" v-else>
       <div class="load-3">
@@ -41,6 +42,7 @@ export default {
       dialogVisible: false,
       isPostLoading: false,
       selectedSort: '',
+      searchQuery: '',
       sortOptions: [
         { value: 'title', name: 'По названию' },
         { value: 'body', name: 'По содержанию' }
@@ -80,6 +82,9 @@ export default {
       return [...this.posts].sort((post1, post2) => {
         return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]);
       });
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter((post) => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
     }
   },
   watch: {}
@@ -110,7 +115,7 @@ export default {
   width: 15px;
   height: 15px;
   border-radius: 15px;
-  background-color: #4b9cdb;
+  background-color: blueviolet;
 }
 .load-3 .line:nth-last-child(1) {
   animation: loadingC 0.6s 0.1s linear infinite;
