85ce8ec40635d8cc5129181d3a22eb63cec0b826
Dmitriifd <idimka9@gmail.com>
2023-04-15 21:26:35 +0500

    loader

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Страница с постами</h1>
     <div class="app__btns">
       <my-button @click="showDialog">Создать пост</my-button>
-      <my-select v-model="selectedSort" :options="sortOptions"/>
+      <my-select v-model="selectedSort" :options="sortOptions" />
     </div>
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
@@ -12,7 +12,14 @@
     <!-- <post-list v-bind:posts="posts"/> -->
     <!-- короткая запись -->
     <post-list :posts="sortedPosts" @remove="removePost" v-if="!isPostLoading" />
-    <div v-else>Loading...</div>
+    <!-- <div v-else>Loading...</div> -->
+     <div class="load-wrapp" v-else>
+      <div class="load-3">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,8 +42,8 @@ export default {
       isPostLoading: false,
       selectedSort: '',
       sortOptions: [
-        {value: 'title', name: 'По названию'},
-        {value: 'body', name: 'По содержанию'},
+        { value: 'title', name: 'По названию' },
+        { value: 'body', name: 'По содержанию' }
       ]
     };
   },
@@ -57,7 +64,7 @@ export default {
         setTimeout(async () => {
           const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
           this.posts = response.data;
-           this.isPostLoading = false;
+          this.isPostLoading = false;
         }, 1000);
       } catch (e) {
         alert('Error');
@@ -71,12 +78,11 @@ export default {
   computed: {
     sortedPosts() {
       return [...this.posts].sort((post1, post2) => {
-        return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
-      })
+        return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]);
+      });
     }
   },
-  watch: {
-  }
+  watch: {}
 };
 </script>
 
@@ -91,5 +97,39 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+.load-wrapp {
+  margin: 0 auto;
+  width: 100px;
+  height: 100px;
+  padding: 20px 20px 20px;
+  border-radius: 5px;
+}
+.line {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  border-radius: 15px;
+  background-color: #4b9cdb;
+}
+.load-3 .line:nth-last-child(1) {
+  animation: loadingC 0.6s 0.1s linear infinite;
+}
+.load-3 .line:nth-last-child(2) {
+  animation: loadingC 0.6s 0.2s linear infinite;
+}
+.load-3 .line:nth-last-child(3) {
+  animation: loadingC 0.6s 0.3s linear infinite;
+}
+@keyframes loadingC {
+  0% {
+    transform: translate(0, 0);
+  }
+  50% {
+    transform: translate(0, 15px);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
 }
 </style>
